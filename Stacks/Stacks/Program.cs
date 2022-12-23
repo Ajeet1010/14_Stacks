57cd919477b1cb7f8a5d941e72ef30a38df84078
ajeet1010 <ajeetpal101999@gmail.com>
2022-12-24 01:45:59 +0530

    Pushing in node

---
+++ Stacks/Stacks/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stacks Problem");
+            Console.WriteLine("Choose to perform:\n" +
+                " 1.Push\n ");
+            Console.WriteLine("Enter the option");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Stack stack = new Stack();
+            switch (num)
+            {
+                case 1:
+                    stack.Push(70);
+                    stack.Push(30);
+                    stack.Push(56);
+                    stack.Display();
+                    break;
+                default:
+                    Console.WriteLine("Enter a valid option");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
