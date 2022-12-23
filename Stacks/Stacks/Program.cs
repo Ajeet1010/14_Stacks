d9424220460a462fe1f29a9d1f0bec9a321f2351
ajeet1010 <ajeetpal101999@gmail.com>
2022-12-24 01:56:52 +0530

    peek and pop from the Stack till it is empty

--- Stacks/Stacks/Program.cs
+++ Stacks/Stacks/Program.cs
@@ -12,7 +12,7 @@ namespace Stacks
         {
             Console.WriteLine("Stacks Problem");
             Console.WriteLine("Choose to perform:\n" +
-                " 1.Push\n ");
+                " 1. Push\n2. Peek and Pop\n ");
             Console.WriteLine("Enter the option");
             int num = Convert.ToInt32(Console.ReadLine());
             Stack stack = new Stack();
@@ -24,6 +24,17 @@ namespace Stacks
                     stack.Push(56);
                     stack.Display();
                     break;
+
+                case 2:
+                    stack.Push(70);
+                    stack.Push(30);
+                    stack.Push(56);
+                    stack.IsEmpty();
+                    stack.Pop();
+                    stack.Peek();
+                    stack.Display();
+                    break;
+
                 default:
                     Console.WriteLine("Enter a valid option");
                     break;
--- Stacks/Stacks/Stacks.cs
+++ Stacks/Stacks/Stacks.cs
@@ -20,6 +20,36 @@ namespace Stacks
             this.top = newNode;
             Console.WriteLine("New Node {0} is added ", newNode.data);
         }
+        //Peek method: It gives the top most element in stack without removing the element.
+        public void Peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("No elements present in the stack");
+                return;
+            }
+            Console.WriteLine("The element after peeking is : " + this.top.data);
+        }
+        //Pop method: It removes the top most element
+        public void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("No elements present in the stack");
+                return;
+            }
+            Peek();
+            this.top = this.top.next;
+        }
+        //Checking the any underflow or overflow occurs(size of stack)
+        public void IsEmpty()                               // IsEmpty: checking stack is empty 
+        {
+            while (this.top != null)
+            {
+                Peek();
+                Pop();
+            }
+        }
         // displaying node
         public void Display()
         {
